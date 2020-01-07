d813a1e83f9da2843073758d534380899ca5a827
deebs <hereisdanbrown@gmail.com>
2020-01-06 17:53:30 -0700

    Added basic framework for the in choice

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var container = $('.container');
 // Second Page Variables:
 // In Variables:
 var q2i = $(".question2i");
+var movieForm = $("form")
 // Out Variables:
 var q2o = $(".question2o");
 
@@ -21,6 +22,11 @@ inBtn.on("click", function(event){
     var situation = $(this).attr("data-choice") + 1;
     renderPage(situation);
 });
+
+movieForm.on("submit", function(event){
+    event.preventDefault();
+    console.log("Submitted the movie");
+})
 // Ajax call for an Out event here
 q2o.on("click", "button", function(event){
     event.preventDefault();
@@ -49,19 +55,19 @@ function renderPage(situation){
             q2o.attr("style", "display: box"); // Displaying
             break;
 
-        case "in1":
-            console.log("in1")
-            h1.text("What movie do you feel like watching?")
-            q1.css("display", "none");
-            q2i.attr("style", "display: box"); // Displaying
-            q2o.css("display", "none");
-            break;
-        
         case "daySelected":
             console.log("daySelected")
             h1.text("Things to do:");
             q1.css("display", "none");
             q2i.css("display", "none");
+            q2o.css("display", "none");
+            break;
+
+        case "in1":
+            console.log("in1")
+            h1.text("What movie do you feel like watching?")
+            q1.css("display", "none");
+            q2i.attr("style", "display: box"); // Displaying
             q2o.css("display", "none");
             break;
 
@@ -77,10 +83,11 @@ function renderPage(situation){
 }
 // This function is going to do the heavy lifting with displaying the AJAX call
 function dayDisplay(date){
+    // I could only get this to work by creating a new div. I would prefer to create this div outside of this dayDisplay, so we can erase it.
     var a2o = $("<div class='row'></div>")
     var exampleH4 = $("<h4>Here is an example header</h4>");
     console.log(exampleH4);
-    var exampleImg = $("<img href='https://cdn.pixabay.com/photo/2019/12/30/20/35/snow-4730565_1280.jpg'>")
+    var exampleImg = $("<img src='https://cdn.pixabay.com/photo/2019/12/30/20/35/snow-4730565_1280.jpg'>")
     var exampleList = $("<ul><li>IDK</li><li>DUDE</li></ul>")
     container.append(a2o);
     a2o.append(exampleH4);
