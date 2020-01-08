3c47330237f5f666e348faad0da1f3d966071db0
deebs <hereisdanbrown@gmail.com>
2020-01-08 10:02:44 -0700

    Rearranged some functions and added some comments.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Variables
+// HTML Variables
 var h1 = $("h1");
 var container = $('.container');
 var eventForm = $("form")
@@ -8,23 +8,39 @@ var now;
 var nowDateTime;
 var selectedDate;
 
+// Ticketmaster Variables
+var nextBtn = $("#next");
+var prevBtn = $("#prev");
+var typeEvent = $("#segment").val();
+var price = $("#price").val();
+var queryDate = $("#date").val();
+var startDate = 0;
 var ticketMasterAPIKey = 'OIxE4IaaAdswnN3Q9eeEnasXqbbJzEnG';
 var page = 0;
 var size = 4; // Make this dynamic
 
-// Implement prev and next button
-
 // Event Listeners
+nextBtn.on("click", function(e){
+    e.preventDefault();
+    page = pageTurn(1, page);
+    // Currently run another
+    // getEvents(page);
+    console.log(page);
+})
+prevBtn.on("click", function(e){
+    e.preventDefault();
+    page = pageTurn(-1, page);
+    // getEvents(page);
+    console.log(page);
+});
 // API
 eventForm.on("submit", function(event){
     event.preventDefault();
-    var price = $("#price").val();
-    var queryDate = $("#date").val();
-    var segment = $("#segment").val();
+    price = $("#price").val();
+    queryDate = $("#date").val();
+    typeEvent = $("#segment").val();
+    page = 0;
     getEvents(page);
-    console.log(price);
-    console.log(segment);
-    console.log(queryDate);
 });
 // Functions
 function renderPage(situation){
@@ -84,56 +100,83 @@ function getEvents(page) {
 
     $('#events-panel').show();
     $('#attraction-panel').hide();
-  
-    if (page < 0) {
-      page = 0;
-      return;
-    }
-    if (page > 0) {
-      if (page > getEvents.json.page.totalPages-1) {
-        page=0;
-      }
-    }
-    
+
+    queryURL = queryURLFiller(typeEvent, startDate, price, size, page);
+
+
     $.ajax({
-      type:"GET",
-      url:`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${ticketMasterAPIKey}&size=${size}&page=${page}`,
-      async:true,
+      type: "GET",
+      url: queryURL, 
+      async: true,
       dataType: "json",
       success: function(json) {
             getEvents.json = json;
-                  showEvents(json);
-               },
+            console.log(getEvents.json);
+            showEvents(json);
+      },
       error: function(xhr, status, err) {
-                  console.log(err);
-               }
+            console.log(err);
+      }
     });
   }
-
-function showEvents(json) {
-    var items = $('#events .list-group-item');
-    items.hide();
-    var events = json._embedded.events;
-    var item = items.first();
-    for (var i=0;i<events.length;i++) {
+  
+  function showEvents(json) {
+      var items = $('#events .list-group-item'); // Targeting our HTML
+      items.hide();
+      var events = json._embedded.events; // Events from the call
+      console.log("In showEvents, here are the json._embedded.events:")
+      console.log(events);
+      var item = items.first();
+    for (var i = 0; i < events.length; i++) {
         item.children('.list-group-item-heading').text(events[i].name);
         item.children('.list-group-item-text').text(events[i].dates.start.localDate);
         try {
-        item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
+            item.children('.venue').text(events[i]._embedded.venues[0].name + " in " + events[i]._embedded.venues[0].city.name);
         } catch (err) {
-        console.log(err);
+            console.log(err);
         }
         item.show();
         item.off("click");
+        // Clicking functionality for each of the items
         item.click(events[i], function(eventObject) {
-        console.log(eventObject.data);
-        try {
-            console.log(eventObject.data._embedded.attractions[0].id);
-            // getAttraction(eventObject.data._embedded.attractions[0].id);
-        } catch (err) {
-        console.log(err);
-        }
+            console.log(eventObject.data);
+            try {
+                console.log(eventObject.data._embedded.attractions[0].id);
+                // getAttraction(eventObject.data._embedded.attractions[0].id);
+            } catch (err) {
+                console.log(err);
+            }
         });
-        item=item.next();
+    item = item.next();
     }
+}
+
+function queryURLFiller(typeEvent, startDate, price, size, page){
+    var queryURL = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${ticketMasterAPIKey}`
+    if (typeEvent !== "undefined"){
+        queryURL += `&classificationName=${typeEvent}`
+    };
+    //if (startDate !== 0){
+    // queryURL += `&startDateTime=${startDate}`; //&endDateTime=${}
+    // Figure out how this works YYYY-MM-DD`
+    //}
+    queryURL += `&size=${size}&page=${page}`;
+    return queryURL;
+};
+
+
+function pageTurn(increment, page){
+    // Fix this. Make it so the page turn only works when there are events populated
+    // if (getEvents.json === undefined){
+    //     return;
+    // };
+    page += increment;
+    if (page < 0) {
+        page = getEvents.json.page.totalPages - 1;
+        return;
+      }
+    if (page > getEvents.json.page.totalPages - 1) {
+        page = 0;
+    }
+    return page;
 }
